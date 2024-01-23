45facb5c2e44220ced537b009d3bff08d5744d89
edwincomeros23 <143491901+edwincomeros23@users.noreply.github.com>
2024-01-23 16:41:00 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <label class="logo" href="">LM Enterprises</label>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Features</a></li>
+            <li><a href="aboutus.php">About Us</a></li>
+            <li><a href="support.php">Support</a></li>
+            <li><a href="#">Contact Us</a></li>
+            </div>
+            </div>
+        </ul>
+    </nav>
+    <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active c-item">
+        <img src="./img/istockphoto-1728515033-1024x1024.jpg" class="d-block w-100 c-img" alt="Slide 1">
+        <div class="carousel-caption top-0 mt-4">
+          <p class="mt-5 fs-3 text-uppercase">Discover new things!</p>
+          <h1 class="display-1 fw-bolder text-capitalize">LM ENTERPRISES</h1>
+        </div>
+      </div>
+      <div class="carousel-item c-item">
+        <img src="./img/istockphoto-1542206733-1024x1024.jpg" class="d-block w-100 c-img" alt="Slide 2">
+        <div class="carousel-caption top-0 mt-4">
+          <p class="text-uppercase fs-3 mt-5">The Items are Affordable!</p>
+          <p class="display-1 fw-bolder text-capitalize">LM ENTERPRISES</p>
+          
+        </div>
+      </div>
+      <div class="carousel-item c-item">
+        <img src="./img/istockphoto-1414378934-1024x1024.jpg" class="d-block w-100 c-img" alt="Slide 3">
+        <div class="carousel-caption top-0 mt-4">
+          <p class="text-uppercase fs-3 mt-5">Moe Options to Choose</p>
+          <p class="display-1 fw-bolder text-capitalize">LM ENTERPRISES</p>
+          
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#hero-carousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#hero-carousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <label class="logo" href="">LM Enterprises</label>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Features</a></li>
+            <li><a href="aboutus.php">About Us</a></li>
+            <li><a href="support.php">Support</a></li>
+            <li><a href="#">Contact Us</a></li>
+            </div>
+            </div>
+        </ul>
+    </nav>
+        <div class="banner-img">
+            <img src="./img/steptodown.com306197 (1).jpg" style="background-image: cover; height:80%; width:100%;">
+<div class="login-container">
+            <h2>Log in</h2>
+            <form id="login-form" action="#" method="post">
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
+                <div class="rem-for">
+                    <a href="">Forgot Password?</a>
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Login">
+                </div>
+                <div class="register">
+                    <p>Don't have an account?<a href="signup.phpN"> Register here</a></p>
+                </div>
+        </div>
+        </form>
+        </div>
+</body>
+</html>
